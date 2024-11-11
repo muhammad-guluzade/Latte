1609a49f5c47e45cd629b9ab69e528b4242d8d18
malai320 <127290622+muhammad-guluzade@users.noreply.github.com>
2024-11-11 18:46:25 +0200

    Update main.py
    Added comments to the functions

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
 from flask import Flask, render_template, request
 
+# Creating an app that runs the entire localhost server
 app = Flask(__name__)
 
+# ROUTES FOR DISPLAYING PAGES
 
-@app.route("/")
+# index.html
+# ========================
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+# ========================
 
 
+# calibration.html
+# ========================
+@app.route('/calibration', methods=["GET"])
+def calibration():
+    return render_template("calibration.html")
+# ========================
+
+
+# test_code.html
+# ========================
+@app.route("/test_code", methods=["GET"])
+def test_code():
+    return render_template("test_code.html")
+# ========================
+
+# ROUTES FOR PROCESSING DATA WITH AJAX
+
+# /store gets the list of 20 dictionaries which have the following structure:
+# {x: float, y: float, t: float}
+# It then stores the x, y coordinates, and time in seconds starting from 01/01/1970
+# in order to have unique time for each gaze point.
+# It stores details in details.txt for now.
+# ========================
 @app.route("/store", methods=["POST"])
 def store():
     with open("details.txt", "a+") as file:
         for dictionary in request.json:
             file.write(f"X: {dictionary['x']}, Y: {dictionary['y']}, Time: {dictionary['t']}s\n")
     return "200"
-
-
-@app.route('/calibration')
-def calibration():
-    return render_template("calibration.html")
-
-
-@app.route("/test_code")
-def test_code():
-    return render_template("test_code.html")
+# ========================
 
 
 if __name__ == "__main__":
